96252fcca0a3be75db239f57975a207bc91d9105
PebblesDV <pebblesdevries@gmail.com>
2023-11-10 13:45:29 +0100

    Added clock

--- main.js
+++ main.js
@@ -22,5 +22,19 @@ function switchTime() {
     showingTimeLeft = true; //sets showingTimeLeft to true
   }
 }
-
 setInterval(switchTime, 5000); //switches between showtime & timeleft every 5 seconds
+
+setInterval(() => {
+  const d = new Date();
+  const hr = d.getHours();
+  const min = d.getMinutes();
+  const sec = d.getSeconds();
+
+  hr_rotation = 30 * hr + min / 2;
+  min_rotation = 6 * min;
+  sec_rotation = 6 * sec;
+
+  hour.style.transform = `rotate(${hr_rotation}deg)`;
+  minute.style.transform = `rotate(${min_rotation}deg)`;
+  second.style.transform = `rotate(${sec_rotation}deg)`;
+}, 1000);
